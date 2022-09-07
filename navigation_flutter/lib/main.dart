986acb7b8a8805e05cecae4b4a791b7d02a91784
Mamoon khan <xsmamoon@gmail.com>
2022-09-08 00:58:41 +0500

    second container review

--- navigation_flutter/lib/main.dart
+++ navigation_flutter/lib/main.dart
@@ -40,28 +40,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Column(
+          children: [
+            Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
+            )
+          ],
+        ));
   }
 }
